f6a3eb65ab18f2cdc3ad3059ef92fe3bb3ea272d
daalekz <dontdissthesonic123@gmail.com>
2022-12-18 18:42:04 +0000

    return list of my own holdings and their current prices

--- example.js
+++ example.js
@@ -7,53 +7,18 @@
 // But we have to do things differently because this example is inside the
 // commsec package.
 const Commsec = require('./index.js');
+const creds = require('./creds.json')
 
 // Everything is wrapped inside an async function so we can 'await'.
 async function run()
 {
 	let commsec = new Commsec();
 
-	// You can also enable debugging if needed.
-	/*
-	let commsec = new Commsec({
-		// This option shows what the library is trying to do.
-		debug: true,
-
-		// This option displays all the JSON server responses, which is
-		// useful when deciphering the API.
-		dumpServerResponses: true,
-	});
-	*/
-
 	// The device ID is allocated upon a successful login.  You should store it
 	// somewhere and supply it on subsequent logins.  It's possible that not doing
 	// so will cause older device IDs to get deleted, breaking the login on the
 	// real app.
 	let deviceId = null;
-
-	// Set the credentials.  You'd normally load these from a secure location,
-	// such as an environment variable or a file outside any git repository.
-	let creds = {
-		"clientId": "12345678",
-		"deviceId": deviceId,
-		"loginType": "password",
-		"password": "secret",
-		// The trading password is optional and not needed to view prices
-		// or holdings.
-		"tradingPassword": "supersecret",
-	};
-
-	// If you have a PIN set up and have remembered the device ID from an earlier
-	// password login, you can log in with the PIN instead.
-	/*
-	let creds = {
-		"clientId": "12345678",
-		"deviceId": "abcdefghijklmnopqrstuvwxyz0123456789%3d",
-		"loginType": "pin",
-		"password": "1234",
-		"tradingPassword": "supersecret",
-	};
-	*/
 
 	// Attempt the login.  If this fails while you're testing, remember to log in
 	// via the web site or app after two failed attempts, to avoid your account
@@ -74,114 +39,33 @@ async function run()
 		console.log('Received a new device ID:', loginResponse.deviceId);
 	}
 
-	// List the trading accounts.
-	loginResponse.accounts.forEach(a => {
-		console.log('Account name:', a.name);
-	});
-
-	// Show any watchlists.
 	try {
-		let w = await commsec.getWatchlists();
-		w.lists.forEach(list => {
-			console.log('Watchlist', list.id);
-			list.items.forEach(i => {
-				console.log(`  ${i.code} @ \$${i.lastPrice} / ${i.volume}\tO:${i.offer} H:${i.high} L:${i.low}`);
-			});
-		});
-	} catch (e) {
-		console.log('Error retrieving watchlist:', e.message);
-	}
-
-	// Show any holdings.
-	try {
-		let h = await commsec.getHoldings();
-		h.entities.forEach(entity => {
+		let myRawHoldings = await commsec.getHoldings();
+		var holdingsForQuery = {};
+		var holdingsForDisplay = {};
+		myRawHoldings.entities.forEach(entity => {
 			console.log('Entity', entity.entityName);
 			entity.accounts.forEach(account => {
-				console.log(' - Account', account.accountNumber, account.entityName);
 				account.holdings.forEach(holding => {
-					console.log(`     ${holding.code}: ${holding.availableUnits} @ ${holding.purchasePrice}`);
+					// the library expects this weird "an object with many kv properties but NOT a list"
+					// so hence the format: holdings = {code1: null, code2: null}
+					// the null is used for hashes which the endpoint returns (check the original example),
+					// kinda a hacky caching thing? 
+					holdingsForQuery[holding.code] = null; 
+					holdingsForDisplay[holding.code + 'buyPrice'] = holding.purchasePrice;
+					holdingsForDisplay[holding.code + 'availableUnits'] = holding.availableUnits;
 				});
 			});
-			console.log(' - Cash accounts:', entity.cashAccounts.length);
-			console.log(' - International accounts:', entity.internationalAccounts.length);
-			console.log(' - Margin loans:', entity.marginLoans.length);
 		});
-	} catch (e) {
-		console.log('Error retrieving holdings:', e.message);
-	}
 
-	// Show any pending or cancelled orders.
-	try {
-		let o = await commsec.getOrders();
-		console.log('Pending orders');
-		o.outstandingOrders.forEach(order => {
-			console.log(` - ${order.orderId}: ${order.stockCode}, ${order.qty} @ ${order.limitPrice}`);
-		});
-		if (o.outstandingOrders.length === 0) console.log(' - None');
-
-		console.log('Cancelled orders');
-		o.cancelledOrders.forEach(order => {
-			console.log(` - ${order.orderId}: ${order.stockCode}, ${order.qty} @ ${order.limitPrice}`);
-		});
-		if (o.cancelledOrders.length === 0) console.log(' - None');
-
-		console.log('Executed orders');
-		o.executedOrders.forEach(order => {
-			console.log(` - ${order.orderId}: ${order.stockCode}, ${order.qty} @ ${order.limitPrice}`);
-		});
-		if (o.executedOrders.length === 0) console.log(' - None');
-	} catch (e) {
-		console.log('Error retrieving orders:', e.message);
-	}
-
-	// Buy some shares.
-	try {
-		let o = await commsec.placeOrder({
-			'code': 'CBA',
-			'orderType': 'BUY',
-			'limitPrice': 1.23,
-			'orderValue': 1.23,
-			'priceType': 'LIMIT',
-			'expiry': '20 DAYS',
-			'quantity': 100,
-		});
-		console.log('Placed order:', o);
-	} catch (e) {
-		console.log('Error placing order:', e.message);
-	}
-
-	// Cancel an order already in the market.
-	try {
-		let o = await commsec.cancelOrder(12345);
-		if (o.orderDidCancel) {
-			console.log('Cancelled order:', o);
-		} else (
-			console.log('Could not cancel order:', o);
-		}
-	} catch (e) {
-		console.log('Error cancelling order:', e.message);
-	}
-
-	// This example shows polling of stocks.  These stocks do not have to be on a
-	// watchlist.  When the value is returned, a hash is supplied.  This hash is
-	// included in subsequent requests so that data is only returned when it has
-	// changed.
-	let stockList = {
-		"ANZ": null,
-		"CBA": null,
-		"NAB": null,
-		"WBC": null,
-	};
-	setInterval(async () => {
-		console.log('Polling stocks');
-		let stockInfosResponse = await commsec.pollStocks(stockList);
+		console.log("Code, Quantity, BuyPrice, CurrentPrice")
+		let stockInfosResponse = await commsec.pollStocks(holdingsForQuery);
 		stockInfosResponse.stockInfos.forEach(stock => {
-			console.log(`[${stock.code}] Updated \$${stock.lastPrice} B:${stock.bid} A:${stock.offer}`);
-			// Record the hash for the next call
-			stockList[stock.code] = stock.hash;
+			console.log(`${stock.code}, ${holdingsForDisplay[stock.code + 'availableUnits']}, ${holdingsForDisplay[stock.code + 'buyPrice']}, \$${stock.lastPrice}`);
 		});
-	}, 2000);
+	} catch (e) {
+		console.log('Error getting current prices of personal hodlings', e.message);
+	}
 }
 
 run();
